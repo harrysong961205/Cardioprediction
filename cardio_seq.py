e9dc7d81050470f9b99018858237ea370947b4b9
harrysong961205 <fkdlzldeja@gmail.com>
2022-05-29 23:17:55 +0900

    find most effective batch, epoch size part 1

--- cardio_seq.py
+++ cardio_seq.py
@@ -6,92 +6,105 @@ dataset = pd.read_csv("/Users/harrysong/Downloads/cardio/cardio_train.csv",sep="
 
 
 # 3. X/y 나누기
-X = dataset.iloc[:,1:-1]
-y = dataset.iloc[:,-1]
+train_list = []
+epoch_level = [250,500,750,1000]
+batch_level = [50,100,200]
+for a in range(0,len(epoch_level) * len(batch_level))
+  X = dataset.iloc[:,1:-1]
+  y = dataset.iloc[:,-1]
+  cnt = epoch_level//len(epoch_level)
+  
 
 
 
-print(X.head(10))
-print(y.head(10))
+  print(X.head(10))
+  print(y.head(10))
 
 
-print(X.shape)
-print(y.shape)
+  print(X.shape)
+  print(y.shape)
 
-# 4. Train set, Test set 나누기
-from sklearn.model_selection import train_test_split
+  # 4. Train set, Test set 나누기
+  from sklearn.model_selection import train_test_split
 
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state=9)
+  X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state=9)
 
-X_val,X_test,y_val,y_test = train_test_split(X_test, y_test, test_size=0.5, random_state=123)
+  X_val,X_test,y_val,y_test = train_test_split(X_test, y_test, test_size=0.5, random_state=123)
 
-print(X_train.shape)
-print(y_train.shape)
+  print(X_train.shape)
+  print(y_train.shape)
 
-print(X_val.shape)
-print(y_val.shape)
+  print(X_val.shape)
+  print(y_val.shape)
 
-print(X_test.shape)
-print(y_test.shape)
+  print(X_test.shape)
+  print(y_test.shape)
 
- 
-# 5. Keras 패키지 가져오기
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-import keras
+  
+  # 5. Keras 패키지 가져오기
+  from keras.models import Sequential
+  from keras.layers import Dense, Dropout
+  import keras
 
-print(keras.__version__)
+  print(keras.__version__)
 
-# 6. MLP 모델 생성
-model = Sequential()
-drop_rate = 0.03
-model.add(Dense(20, input_dim=11, activation='relu'))
-#model.add(keras.layers.Dropout(drop_rate))
-model.add(Dense(16, activation='relu'))
-#model.add(keras.layers.Dropout(drop_rate))
-model.add(Dense(8, activation='relu'))
-#model.add(keras.layers.Dropout(drop_rate))
-model.add(Dense(1, activation='sigmoid'))
+  # 6. MLP 모델 생성
+  model = Sequential()
+  drop_rate = 0.03
+  model.add(Dense(256, input_dim=11, activation='relu'))
+  #model.add(keras.layers.Dropout(drop_rate))
+  model.add(Dense(64, activation='relu'))
+  model.add(Dense(16, activation='relu'))
+  #model.add(keras.layers.Dropout(drop_rate))
+  #model.add(keras.layers.Dropout(drop_rate))
+  model.add(Dense(1, activation='sigmoid'))
 
-print(model.summary())
+  # 6-2 . early_stopping
+  from keras.callbacks import EarlyStopping
+  from keras.callbacks import ModelCheckpoint
+  mc = ModelCheckpoint('model', monitor='accuracy', mode='max', save_best_only=True)
+  early_stopping = EarlyStopping(monitor='val_acc', min_delta = 0.0005, patience = 20,mode = 'auto')
 
-# 7. Compile - Optimizer, Loss function 설정
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+  print(model.summary())
+
+  # 7. Compile - Optimizer, Loss function 설정
+  model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 
-# 6. 학습시키기
 
-batch_size = 200
-epochs = 1000
+  # 6. 학습시키기
 
-history = model.fit(X_train, y_train, epochs=epochs, 
-                    batch_size=batch_size, 
-                    validation_data=(X_val, y_val), shuffle=True, verbose=1)
-                    
-                    
-# 7. 모델 평가하기
-train_accuracy = model.evaluate(X_train, y_train)
-test_accuracy = model.evaluate(X_test, y_test)
+  batch_size = batch_level
+  epochs = epoch_level
 
-print(train_accuracy)
-print(test_accuracy)
+  history = model.fit(X_train, y_train, epochs=epochs, 
+                      batch_size=batch_size, 
+                      validation_data=(X_val, y_val), shuffle=True, verbose=1,
+                      callbacks=[early_stopping])
+                      
+                      
+  # 7. 모델 평가하기a
+  train_accuracy = model.evaluate(X_train, y_train)
+  test_accuracy = model.evaluate(X_test, y_test)
+  print(train_accuracy)
+  print(test_accuracy)
 
- 
-# 10. 학습 시각화하기
-import matplotlib.pyplot as plt
+  
+  # 10. 학습 시각화하기
+  import matplotlib.pyplot as plt
 
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('Accuracy')
-plt.ylabel('epoch')
-plt.xlabel('accuracy')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
+  plt.plot(history.history['accuracy'])
+  plt.plot(history.history['val_accuracy'])
+  plt.title('Accuracy')
+  plt.ylabel('accuracy')
+  plt.xlabel('epoch')
+  plt.legend(['train', 'test'], loc='upper left')
+  plt.show()
 
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Loss')
-plt.ylabel('epoch')
-plt.xlabel('loss')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
+  plt.plot(history.history['loss'])
+  plt.plot(history.history['val_loss'])
+  plt.title('Loss')
+  plt.ylabel('loss')
+  plt.xlabel('epoch')
+  plt.legend(['train', 'test'], loc='upper left')
+  plt.show()
